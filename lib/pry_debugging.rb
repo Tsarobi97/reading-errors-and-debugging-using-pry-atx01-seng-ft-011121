db39c0919034ef41f0f2c2c34668f2d3bf9cc74f
Robert Obi <robobi97@googlemail.com>
2020-12-24 06:58:25 +0000

    Automatically backed up by Learn

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -3,3 +3,14 @@ def plus_two(num)
 	num + 2
 	return num
 end
+
+
+require 'pry'
+require './lib/pry_debugging.rb'
+
+describe "#plus_two" do 
+	it "takes in a number as an argument and returns the sum of that number and 2" do
+		expect(plus_two(3)).to eq(5)
+	end
+
+end
